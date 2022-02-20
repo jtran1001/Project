1f5b3ade27325aced770e105a7d306a37a23c883
jtran1001 <dtran@unb.ca>
2022-02-19 23:41:08 -0400

    Update Camera

--- RollaBallWithGit/Assets/Scripts/PlayerController.cs
+++ RollaBallWithGit/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementZ;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //accept value passed in from the Inspector of RigidBody Component
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMove(InputValue movementValue)
     {
-        
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementZ = movementVector.y;
+    }
+
+    //physic that needs to be updated per frame
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementZ);
+        rb.AddForce(movement * speed);
     }
 }
