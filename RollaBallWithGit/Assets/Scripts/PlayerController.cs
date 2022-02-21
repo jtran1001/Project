3b6d1e5b4740c1da1865ce62dd3bd32bd756331d
jtran1001 <dtran@unb.ca>
2022-02-20 21:18:46 -0400

    RollABall finished

--- RollaBallWithGit/Assets/Scripts/PlayerController.cs
+++ RollaBallWithGit/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,12 +10,20 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private float movementX;
     private float movementZ;
+    //This is for the pick up count
+    private int count;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     // Start is called before the first frame update
     void Start()
     {
         //accept value passed in from the Inspector of RigidBody Component
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.gameObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -25,10 +34,34 @@ public class PlayerController : MonoBehaviour
         movementZ = movementVector.y;
     }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+
+    void SetWinText()
+    {
+        winTextObject.gameObject.SetActive(true);
+    }
+
     //physic that needs to be updated per frame
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementZ);
         rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick-Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+            if(count >= 6)
+            {
+                SetWinText();
+            }
+        }
     }
 }
