8c561635bdabf04536805ccc8223f99ff3a4648a
jtran1001 <dtran@unb.ca>
2022-02-21 00:27:26 -0400

    Update Code
    Code now run well-using arrow keys
    
    Every time hit S, timer reset back to 30

--- RollaBallWithGit/Assets/Scripts/PlayerController.cs
+++ RollaBallWithGit/Assets/Scripts/PlayerController.cs
@@ -12,26 +12,42 @@ public class PlayerController : MonoBehaviour
     private float movementZ;
     //This is for the pick up count
     private int count;
+    //count text
     public TextMeshProUGUI countText;
+    //win text
     public GameObject winTextObject;
+    //countDown text
+    public TextMeshProUGUI countDown;
+    private int CountDown;
+    //lose text
+    public GameObject loseTextObject;
+    //welcome Text
+    public GameObject welcomeText;
 
+    private int timeSinceLoad;
+
+    private bool gameStart = false;
     // Start is called before the first frame update
     void Start()
     {
         //accept value passed in from the Inspector of RigidBody Component
         rb = GetComponent<Rigidbody>();
         count = 0;
-
         SetCountText();
         winTextObject.gameObject.SetActive(false);
+        loseTextObject.gameObject.SetActive(false);
+        welcomeText.gameObject.SetActive(true);
     }
 
     void OnMove(InputValue movementValue)
     {
-        Vector2 movementVector = movementValue.Get<Vector2>();
+        if (gameStart)
+        {
+            Vector2 movementVector = movementValue.Get<Vector2>();
 
-        movementX = movementVector.x;
-        movementZ = movementVector.y;
+            movementX = movementVector.x;
+            movementZ = movementVector.y;
+        }
     }
 
     void SetCountText()
@@ -42,7 +58,48 @@ public class PlayerController : MonoBehaviour
     void SetWinText()
     {
         winTextObject.gameObject.SetActive(true);
+        gameStart = false;
     }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            gameStart = true;
+            welcomeText.gameObject.SetActive(false);
+            //This line determines when the game start counting down
+            //need to find a better place to fit it
+            //if placed here, time resets everytime hit S button
+            timeSinceLoad = (int)Time.timeSinceLevelLoad;
+        }
+        if (gameStart)
+        {
+            SetCountDownText();
+        }
+        
+    }
+
+    void SetCountDownText()
+    {
+        CountDown = 30 - ((int)Time.timeSinceLevelLoad - timeSinceLoad);
+        if (CountDown <= 0)
+        {
+            CountDown = 0;
+        }
+        if (CountDown <= 0 && count < 6)
+        {
+            SetLoseText();
+            gameStart = false;
+        }
+        countDown.text = "Time Left: " + CountDown;
+ 
+    }
+
+    void SetLoseText()
+    {
+        loseTextObject.gameObject.SetActive(true);
+    }
+
 
     //physic that needs to be updated per frame
     void FixedUpdate()
@@ -61,6 +118,7 @@ public class PlayerController : MonoBehaviour
             if(count >= 6)
             {
                 SetWinText();
+                gameStart = false;
             }
         }
     }
